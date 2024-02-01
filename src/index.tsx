73aa1eb48221495dbb298c691ff9c514454bea43
Mika Kanayama <akasjiron@gmail.com>
2024-02-01 09:52:36 +0900

    feat: lint設定追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// import App from './App';
 
 const App = () => <div>Hello, React with TypeScript!</div>;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
