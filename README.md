c0bf0a89905863a957ce9164fbdf2874e7d69136
Mika Kanayama <akasjiron@gmail.com>
2024-02-01 12:32:34 +0900

    feat: lint設定 format追加

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # how to run the app
--  docker-compose up
+
+- docker-compose up
 
 - localhost:8080 にアクセス
 
@@ -7,4 +8,13 @@
 
 - % docker-compose -f docker-compose.storybook.yml up
 - localhost:6006 にアクセス
+
+## その他コマンド
+- docker-compose up それをした後で、別タブなどで docker exec -it react_base-react-app-1  bash とするとコンテナの中の処理が可能
+
+#### コードチェック
+- npm run lint
+
+#### コード整形
+- npm run format
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 // import App from './App';
 
-const App = () => <div>Hello, React with TypeScript!</div>;
+const App = () => <div>Hello, React with TypeScript!</div>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<App />)
